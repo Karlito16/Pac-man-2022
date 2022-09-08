5bfe7fdab5b1f1725bcc5a8a6f76f9f48420b69f
Karlito <karlo.dimjasevic@gmail.com>
2022-09-09 01:43:55 +0200

    Added first basic functionalities for map to be drawn on the screen (update and draw methods). Added constructor and few basic attributes, and getters.
    Signed-off-by: Karlito <karlo.dimjasevic@gmail.com>

--- src/map/map_.py
+++ src/map/map_.py
@@ -3,20 +3,60 @@
 # author: Karlo Dimjašević
 
 
-import src.utils as utils
 from .map_parser import MapParser
-from src.utils import MapFile
+import src.utils as utils
+import src.map.components as components
+
+import pygame
+from typing import Any
 
 
-class Map(object):
+class Map(pygame.Surface):
     """
     Map class.
     """
 
-    def __init__(self, name):
+    def __init__(self, name, size, nodes, grid_slots_wall, grid_slot_size):
         """
         Constructor.
         """
+        self._name = name
+        self._size = size
+        self._nodes = nodes
+        self._walls = components.Walls(grid_slots_wall=grid_slots_wall, grid_slot_size=grid_slot_size)
+        super().__init__(self._size)
+        self.rect = self.get_rect()
+        self.rect.midtop = (utils.WIN_WIDTH.value // 2, utils.WIN_HEIGHT.value * utils.MAP_MARGIN_TOP_PERCENTAGE.value)
+
+    @property
+    def name(self):
+        """Getter."""
+        return self._name
+
+    @property
+    def size(self):
+        """Getter."""
+        return self._size
+
+    @property
+    def nodes(self):
+        """Getter."""
+        return self._nodes
+
+    @property
+    def walls(self):
+        """Getter."""
+        return self._walls
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        """Overrides the method in sprite class."""
+        pass
+
+    def draw(self, surface):
+        """Draws the map."""
+        self.walls.draw(surface=self)
+
+        surface.blit(self, self.rect)
 
 
 def load_maps():
@@ -25,7 +65,16 @@ def load_maps():
     :return: (<class 'Map'>)
     """
     for map_name in utils.MAP_NAMES.value:
-        file_data = MapFile.load(file_name=map_name)
+        file_data = utils.MapFile.load(file_name=map_name)
         parsed_data = MapParser.parse(data=file_data)
         if parsed_data == -1:
             raise TypeError(f"file content invalid:\nfile: {map_name}")
+
+        grid_size, nodes, walls = parsed_data
+        map_width, map_height, grid_slot_size = utils.calculate_map_dimensions(grid_size=grid_size)
+        yield Map(
+            name=map_name,
+            nodes=nodes,
+            size=(map_width, map_height),
+            grid_slot_size=grid_slot_size,
+            grid_slots_wall=walls)
